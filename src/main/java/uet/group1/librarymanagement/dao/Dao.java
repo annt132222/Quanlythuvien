79fdb14e2b1be8ac826ec9b37c9ba9dec9aab060
An Nguyen <nguyentienan1322@gmail.com>
2025-05-10 03:34:48 +0700

    Add DAO interface

--- src/main/java/uet/group1/librarymanagement/dao/Dao.java
+++ src/main/java/uet/group1/librarymanagement/dao/Dao.java
@@ -1,4 +1,12 @@
 package uet.group1.librarymanagement.dao;
 
-public interface Dao {
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao<T, K> {
+    Optional<T> findById(K id);
+    List<T> findAll();
+    boolean insert(T entity);
+    boolean update(T entity);
+    boolean delete(K id);
 }
